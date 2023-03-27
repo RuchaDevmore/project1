8dbd1d702192e56cdc37105202bf6e3ada239afe
RuchaDevmore <128315267+RuchaDevmore@users.noreply.github.com>
2023-03-27 11:02:38 +0530

    update 2

---
+++ DatatypeExample2.java
@@ -0,0 +1,24 @@
+package day1;
+//datatype 
+// 1 primitive datatype   2 non primitive datatype
+// 8 primitive DT 
+public class DatatypeExample2 {
+
+	public static void main(String[] args) {
+		
+        byte b=22;
+		int x=10;
+		short p=32767;
+		long k=33;
+		float a=44.77f;
+		double b1=444.24d;
+		char c='a';
+		boolean m=true;
+		
+		
+		int m1=33000;
+		
+      
+	}
+
+}
---
+++ Exam1.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		int x,y;
+		x=0;
+  		
+
+	}
+
+}
