a5f85ac901b05869abd10a90458c3e481b1883fa
RuchaDevmore <128315267+RuchaDevmore@users.noreply.github.com>
2023-03-27 10:54:04 +0530

    1 file uploaded

---
+++ DatatypeExample.java
@@ -0,0 +1,44 @@
+package day1;
+//variables 
+//1) local variable
+//2) class variable
+//3) instance variable 
+// no global variables in java 
+
+ //int k=20;  compilation error  no global variables allowed
+
+public class DatatypeExample {
+
+	int y; //instance variable
+	//system assign default values to instance variable 
+	// so int - 0
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		int a=10; // local variable 
+		
+		{
+		  int b=20;	//local variable
+		  System.out.println(b);
+		}
+		
+	
+		
+		int p;
+		//System.out.println(p);// local variable must be initialized before using
+		// no garbage values in java 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
